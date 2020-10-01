39ca0886b2848b690ad0c713aab1c9114d19097f
noname-r <noname-r@ya.ru>
2020-10-01 16:49:31 +0300

    part6.2

---
+++ Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using myTestASPNETCoreSite.Domain;
+
+namespace myTestASPNETCoreSite.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems());
+        } 
+    }
+}
